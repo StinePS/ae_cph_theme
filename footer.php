9b90fe33393c51664691401de6d0d866a4c5fb62
StinePS <stin039m@stud.kea.dk>
2021-05-21 17:40:28 +0200

    First commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.2
+ */
+?>
+
+<footer>
+
+	<a href="#">Handelsbetingelser</a>
+	<p>CVR. 42358347</p>
+
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
